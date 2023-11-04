a40ce5fd2abe287aea23c5ad3c9c70cfb345cd95
SwastikaPradhan <93637898+SwastikaPradhan@users.noreply.github.com>
2023-11-04 19:13:41 +0000

    datatypes

---
+++ 01.basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountemail="swastika@gmail"
+var accountpassword="12345";
+accountcity="jaipur"
+
+//accountId =2 cant be right
+
+
+accountemail="swati@gmail.com"
+accountpassword="678954"
+accountcity="bengaluru"
+
+/*Dont use var because it create issue in block scope and functional scope */
+console.log(accountId);
+console.table([accountId,accountemail,accountcity,accountpassword]);
+
---
+++ 01.basics/02_datatype.js
@@ -0,0 +1,12 @@
+let age=18;
+let isloggedIn=false;
+let nameofagirl="swati";
+let state=null;
+//number =>2 to power 53
+//bigint
+//string =>" "
+//boolean => true/false
+
+//null=standalone value
+//undefined;
+console.log(typeof state);
