a2231e46122aaac62086fe5a3a071e8297fc3296
SwastikaPradhan <93637898+SwastikaPradhan@users.noreply.github.com>
2023-11-04 20:51:23 +0000

    converationoperation

---
+++ 01.basics/03_conversionoperation.js
@@ -0,0 +1,31 @@
+let score="33";
+let valueInnumber=Number(score);
+//console.log(typeof(valueInnumber));
+//"33"=>33
+//"33abc"=>NaN(not a number)
+//true=>1;false=>0
+let isloggedin="";
+let booleanisloggedin=Boolean(isloggedin);
+//console.log(typeof(booleanisloggedin));
+//console.log(booleanisloggedin);
+
+//""->false
+
+//" "->true
+
+//"swati"->true
+
+//"0"->false
+
+//"1"->true;
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+
+
+//*****************************operations************************************* */
+let value=3;
+let negvalue=-value;
+console.log(negvalue);
+//study link:-https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
